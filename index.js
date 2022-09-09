faeb0fa32fcba4b21793086d73b0419fea29f508
ifgabriel <gabrielmirandap.ti@gmail.com>
2022-09-09 13:42:05 -0300

    fix toast

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id)
+function drag(event) {
+  event.dataTransfer.setData('text', event.target.id)
 }
 
-function allowDrop(ev) {
-  ev.preventDefault()
+function allowDrop(event) {
+  event.preventDefault()
+
+  if (event.target.classList.contains('list')) {
+    event.target.classList.add('list-hover')
+  }
 }
 
-function drop(ev) {
-  ev.preventDefault()
+function drop(event) {
+  event.preventDefault()
+  
+  const data = event.dataTransfer.getData('text')
+  event.currentTarget.appendChild(document.getElementById(data))
 
-  const data = ev.dataTransfer.getData("text")
-
-  ev.currentTarget.appendChild(document.getElementById(data))
+  if (event.target.classList.contains('list-hover')) {
+    event.target.classList.remove('list-hover')
+  }
 }
 
 function handleShowModal() {
-  const overlay = document.getElementById("overlay")
+  const overlay = document.getElementById('overlay')
 
-  if (overlay.style.display === "none") {
-    overlay.style.display = "flex"
+  if (overlay.style.display === 'none') {
+    overlay.style.display = 'flex'
   } else {
-    overlay.style.display = "none"
+    overlay.style.display = 'none'
   }
 }
 
@@ -31,28 +38,24 @@ function handleCloseModal(e) {
 }
 
 function handleList(initialStatus) {
-  const listTodo = document.getElementById("list-todo")
-  const listDoing = document.getElementById("list-doing")
-  const listDone = document.getElementById("list-done")
+  if (initialStatus === 'todo') return document.getElementById('list-todo')
+  if (initialStatus === 'doing') return document.getElementById('list-doing')
+  if (initialStatus === 'done') return document.getElementById('list-done')
 
-  if (initialStatus === 'todo') return listTodo
-  if (initialStatus === 'doing') return listDoing
-  if (initialStatus === 'done') return listDone
-
-  return listTodo
+  return document.getElementById('list-todo')
 }
 
 function handleCreateTask() {
-  const title = document.getElementById("task-name").value
-  const label = document.getElementById("task-label").value
-  const initialStatus = document.getElementById("task-status").value
-  const description = document.getElementById("task-description").value
+  const title = document.getElementById('task-name').value
+  const label = document.getElementById('task-label').value
+  const initialStatus = document.getElementById('task-status').value
+  const description = document.getElementById('task-description').value
 
   const list = handleList(initialStatus)
 
   if (title && description && initialStatus && label) {
     list.innerHTML += `
-      <li class="task" id="${title.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+      <li class='task' id='${Math.random().toString(36)}' draggable='true' ondragstart='drag(event)'>
           <span class='label ${label}'>${label}</span>
           <span>${title}</span>
           <span>${description}</span>
@@ -67,18 +70,18 @@ function handleCreateTask() {
 }
 
 function handleToast(status) {
-  const toast = document.getElementById("toast-list")
+  const toast = document.getElementById('toast-list')
 
   if (status === 'success') {
     toast.innerHTML += `
-      <li class="toast success">
+      <li class='toast success'>
           <span class='toast-title'>Ação concluída!</span>
           <span class='toast-description'>Nova tarefa cadastrada.</span>
       </li>
     `
   } else {
     toast.innerHTML += `
-      <li class="toast error">
+      <li class='toast error'>
           <span class='toast-title'>Tivemos um problema!</span>
           <span class='toast-description'>Preencha todos os campos.</span>
       </li>
